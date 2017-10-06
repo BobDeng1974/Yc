0a490ace7cfdec5d2fff6c3039f1de68f434d160
Gavin Howard <project.lfyre@gmail.com>
2017-10-06 12:29:14 -0600

    Adjust the message again

--- src/yc.c
+++ src/yc.c
@@ -42,7 +42,7 @@
 void _y_assert(const char* expr_str, bool expr, const char* file, int line, const char* msg) {
 
 	if (!expr) {
-		fprintf(stderr, "Assert failed: %s\n\tExpected: %s\n\tSource:   %s:%d\n", msg, expr_str, file, line);
+		fprintf(stderr, "Assert failed: %s\n\tExpected: %s\n\tSource:   %s:%d\n\n", msg, expr_str, file, line);
 		abort();
 	}
 }
